43f5b84f416a4f862ecb985f495a6098dcdd0008
ukidoshi <nsaryglar200@gmail.com>
2022-03-15 18:43:27 +0300

    style changed

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <string>
 
+<<<<<<< HEAD
 int main(){
 //какие то комменты
 	std::cout<<"hello world!"<<std::endl;
 	string name;
 	std::cin>>name;
 	std::cout<<"Hello world from "<<name<<std::endl;
+=======
+using namespace std;
+
+int
+main()
+{
+  cout << "hello world!" << endl;
+  string name;
+  cin >> name;
+  cout << "Hello world from " << name << endl;
+>>>>>>> d7b4da0 (style changed)
 }
+
